94bccb8dc69186b892775af064eed3f1e198799a
hamzaboutaleb <boutaleb.hm@gmail.com>
2026-06-17 18:44:26 +0100

    Implement Socket class methods for TCP socket operations

--- include/network/Socket.hpp
+++ include/network/Socket.hpp
@@ -8,19 +8,18 @@ class Socket
 
 public:
   Socket(int fd);
-  Socket(const Socket& other);
-  Socket& operator=(const Socket& other);
+  Socket(const Socket &other);
+  Socket &operator=(const Socket &other);
   ~Socket();
 
-  static Socket* createTcp();
+  static Socket *createTcp();
 
-  void     setNonBlocking();
-  void     bind(int port);
-  void     listen(int backlog);
-  Socket*  accept();
-  void     connect(const char* host, int port);
-  IoResult send(const void* buf, size_t len);
-  IoResult recv(void* buf, size_t len);
+  void setNonBlocking();
+  void bind(int port);
+  void listen(int backlog);
+  Socket *accept();
+  IoResult send(const void *buf, size_t len);
+  IoResult recv(void *buf, size_t len);
 
   int fd() const;
 };
---
+++ src/network/Socket.cpp
@@ -0,0 +1,108 @@
+#include "network/Socket.hpp"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+#include <cstring>
+#include <stdexcept>
+
+Socket::Socket(int fd) : _fd(fd) {}
+
+Socket::Socket(const Socket &other) : _fd(dup(other._fd)) {}
+
+Socket &Socket::operator=(const Socket &other)
+{
+  if (this != &other)
+  {
+    if (_fd != -1)
+      close(_fd);
+    _fd = dup(other._fd);
+  }
+  return *this;
+}
+
+Socket::~Socket()
+{
+  if (_fd != -1)
+    close(_fd);
+}
+
+int Socket::fd() const
+{
+  return _fd;
+}
+
+Socket *Socket::createTcp()
+{
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd == -1)
+    throw std::runtime_error(std::strerror(errno));
+
+  int opt = 1; // for setting resuaddr. when program shutdown we can resue immediatly
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+  {
+    close(fd);
+    throw std::runtime_error(std::strerror(errno));
+  }
+
+  return new Socket(fd);
+}
+
+void Socket::setNonBlocking()
+{
+  int flags = fcntl(_fd, F_GETFL, 0); // we get old flags so that we will not lose it when we set nonblock flag
+  if (flags == -1 || fcntl(_fd, F_SETFL, flags | O_NONBLOCK) == -1)
+    throw std::runtime_error(std::strerror(errno));
+}
+
+void Socket::bind(int port)
+{
+  sockaddr_in addr;
+  std::memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(port);
+
+  if (::bind(_fd, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) == -1)
+    throw std::runtime_error(std::strerror(errno));
+}
+
+void Socket::listen(int backlog) // queue size
+{
+  if (::listen(_fd, backlog) == -1)
+    throw std::runtime_error(std::strerror(errno));
+}
+
+Socket *Socket::accept()
+{
+  int clientFd = ::accept(_fd, NULL, NULL);
+  if (clientFd == -1)
+    throw std::runtime_error(std::strerror(errno));
+  return new Socket(clientFd);
+}
+
+IoResult Socket::send(const void *buf, size_t len)
+{
+  ssize_t n = ::send(_fd, buf, len, 0);
+  if (n > 0)
+    return IoResult(IO_OK, static_cast<size_t>(n), 0);
+  if (n == 0)
+    return IoResult(IO_CLOSED, 0, 0);
+  if (errno == EAGAIN || errno == EWOULDBLOCK)
+    return IoResult(IO_WOULDBLOCK, 0, errno);
+  return IoResult(IO_ERROR, 0, errno);
+}
+
+IoResult Socket::recv(void *buf, size_t len)
+{
+  ssize_t n = ::recv(_fd, buf, len, 0);
+  if (n > 0)
+    return IoResult(IO_OK, static_cast<size_t>(n), 0);
+  if (n == 0)
+    return IoResult(IO_CLOSED, 0, 0);
+  if (errno == EAGAIN || errno == EWOULDBLOCK)
+    return IoResult(IO_WOULDBLOCK, 0, errno);
+  return IoResult(IO_ERROR, 0, errno);
+}
