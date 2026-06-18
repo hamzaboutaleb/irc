ce7996fe9a507f9d0cef0fe6c27149dbfa5e7185
hamzaboutaleb <boutaleb.hm@gmail.com>
2026-06-18 10:22:12 +0100

    Implement Buffer class with methods for message handling and data management

---
+++ include/Buffer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class Buffer
+{
+  std::string _data;
+
+public:
+  void        append(const char* data, size_t len);
+  bool        hasMessage() const;
+  std::string extractMessage();
+};
---
+++ src/Buffer.cpp
@@ -0,0 +1,19 @@
+#include "Buffer.hpp"
+
+void Buffer::append(const char* data, size_t len)
+{
+  _data.append(data, len);
+}
+
+bool Buffer::hasMessage() const
+{
+  return _data.find("\r\n") != std::string::npos;
+}
+
+std::string Buffer::extractMessage()
+{
+  size_t pos = _data.find("\r\n");
+  std::string message = _data.substr(0, pos);
+  _data.erase(0, pos + 2);
+  return message;
+}
