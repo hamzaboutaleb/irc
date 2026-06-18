36354778597d636441df8921744864633bb13885
hamzaboutaleb <boutaleb.hm@gmail.com>
2026-06-18 18:21:40 +0100

    Refactor Replies class to use SERVER macro for response formatting

--- src/commands/Replies.cpp
+++ src/commands/Replies.cpp
@@ -1,44 +1,46 @@
 #include "commands/Replies.hpp"
 
+#define SERVER "localhost"
+
 namespace Replies
 {
   std::string needMoreParams(const std::string &nick, const std::string &cmd)
   {
-    return ":" + nick + " 461 " + nick + " " + cmd + " :Not enough parameters\r\n";
+    return ":" SERVER " 461 " + nick + " " + cmd + " :Not enough parameters\r\n";
   }
 
   std::string alreadyRegistered(const std::string &nick)
   {
-    return ":" + nick + " 462 " + nick + " :You may not reregister\r\n";
+    return ":" SERVER " 462 " + nick + " :You may not reregister\r\n";
   }
 
   std::string passwdMismatch(const std::string &nick)
   {
-    return ":" + nick + " 464 " + nick + " :Password incorrect\r\n";
+    return ":" SERVER " 464 " + nick + " :Password incorrect\r\n";
   }
 
   std::string welcome(const std::string &nick, const std::string &user, const std::string &host)
   {
-    return ":server 001 " + nick + " :Welcome to the IRC server " + nick + "!" + user + "@" + host + "\r\n";
+    return ":" SERVER " 001 " + nick + " :Welcome to the IRC server " + nick + "!" + user + "@" + host + "\r\n";
   }
 
   std::string notRegistered(const std::string &nick)
   {
-    return ":" + nick + " 451 " + nick + " :You have not registered\r\n";
+    return ":" SERVER " 451 " + nick + " :You have not registered\r\n";
   }
 
   std::string erroneusNickname(const std::string &nick)
   {
-    return ":" + nick + " 432 " + nick + " :Erroneous nickname\r\n";
+    return ":" SERVER " 432 " + nick + " :Erroneous nickname\r\n";
   }
 
   std::string nicknameInUse(const std::string &nick)
   {
-    return ":" + nick + " 433 * " + nick + " :Nickname is already in use\r\n";
+    return ":" SERVER " 433 " + nick + " :Nickname is already in use\r\n";
   }
 
   std::string noNicknameGiven()
   {
-    return ":* 431 * :No nickname given\r\n";
+    return ":" SERVER " 431 * :No nickname given\r\n";
   }
 }
