456dd261c0559742e20734e1e37788847ae792dd
hamzaboutaleb <boutaleb.hm@gmail.com>
2026-06-18 17:31:58 +0100

    Add ClientInfo class to manage client state and information

---
+++ include/ClientInfo.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+
+enum ClientState
+{
+  CONNECTING,
+  REGISTERED
+};
+
+class ClientInfo
+{
+  ClientState _state;
+  std::string _nickname;
+  std::string _username;
+  std::string _realname;
+  bool _isOper;
+  bool _passReceived;
+
+public:
+  ClientInfo();
+
+  ClientState state() const;
+  const std::string &nickname() const;
+  const std::string &username() const;
+  const std::string &realname() const;
+  bool isOper() const;
+  bool passReceived() const;
+  bool isRegistered() const;
+
+  void setNickname(const std::string &nickname);
+  void setUsername(const std::string &username);
+  void setRealname(const std::string &realname);
+  void markPassReceived();
+  void markRegistered();
+  void promoteToOper();
+};
---
+++ src/ClientInfo.cpp
@@ -0,0 +1,26 @@
+#include "ClientInfo.hpp"
+
+ClientInfo::ClientInfo()
+    : _state(CONNECTING), _isOper(false), _passReceived(false) {}
+
+ClientState ClientInfo::state() const { return _state; }
+const std::string &ClientInfo::nickname() const { return _nickname; }
+const std::string &ClientInfo::username() const { return _username; }
+const std::string &ClientInfo::realname() const { return _realname; }
+bool ClientInfo::isOper() const { return _isOper; }
+bool ClientInfo::passReceived() const { return _passReceived; }
+
+bool ClientInfo::isRegistered() const
+{
+  return _state == REGISTERED;
+}
+
+void ClientInfo::setNickname(const std::string &nickname) { _nickname = nickname; }
+void ClientInfo::setUsername(const std::string &username) { _username = username; }
+void ClientInfo::setRealname(const std::string &realname) { _realname = realname; }
+
+void ClientInfo::markPassReceived() { _passReceived = true; }
+
+void ClientInfo::markRegistered() { _state = REGISTERED; }
+
+void ClientInfo::promoteToOper() { _isOper = true; }
