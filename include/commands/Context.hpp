dd17ebbac7e8cc12f8ffb9da3c59aa7069567082
hamzaboutaleb <boutaleb.hm@gmail.com>
2026-06-19 10:45:34 +0100

    Add channels map to Context struct for improved command handling

--- include/commands/Context.hpp
+++ include/commands/Context.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include "core/Client.hpp"
+#include "core/Channel.hpp"
 #include <map>
 #include <string>
 
 struct Context
 {
   std::map<int, Client *> &clients;
+  std::map<std::string, Channel *> &channels;
   const std::string &password;
 
   Context(std::map<int, Client *> &clients,
+          std::map<std::string, Channel *> &channels,
           const std::string &password)
-      : clients(clients), password(password) {}
+      : clients(clients), channels(channels), password(password) {}
 };
