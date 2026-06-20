14b88c8ff1ea66ed5f8c15b64a47f2bb23055cce
hamzaboutaleb <boutaleb.hm@gmail.com>
2026-06-20 09:41:32 +0100

    fix: validate password charset in parseArgs - reject spaces and control chars

--- src/core/Args.cpp
+++ src/core/Args.cpp
@@ -1,27 +1,33 @@
 #include "core/Args.hpp"
 #include <cstdlib>
+#include <cctype>
 #include <stdexcept>
 #include <string>
 
-Args parseArgs(int argc, char** argv)
+Args parseArgs(int argc, char **argv)
 {
   if (argc != 3)
     throw std::runtime_error("usage: ./irc <port> <password>");
 
-  char* end;
-  long  port = std::strtol(argv[1], &end, 10);
+  char *end;
+  long port = std::strtol(argv[1], &end, 10);
 
   if (*end != '\0')
     throw std::runtime_error("port must be a number");
   if (port < 1024 || port > 65535)
     throw std::runtime_error("port must be between 1024 and 65535");
-
   std::string password = argv[2];
   if (password.empty())
     throw std::runtime_error("password cannot be empty");
+  for (size_t i = 0; i < password.size(); ++i)
+  {
+    unsigned char c = static_cast<unsigned char>(password[i]);
+    if (!std::isalnum(c) && !std::ispunct(c))
+      throw std::runtime_error("password must contain only letters, digits and symbols");
+  }
 
   Args args;
-  args.port     = static_cast<int>(port);
+  args.port = static_cast<int>(port);
   args.password = password;
   return args;
 }
