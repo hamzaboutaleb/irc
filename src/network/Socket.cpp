08c50827efcb20583fdf12255175688a6e147639
hamzaboutaleb <boutaleb.hm@gmail.com>
2026-06-23 09:36:48 +0100

    feat: update send method to handle EPIPE and ECONNRESET errors correctly

--- src/network/Socket.cpp
+++ src/network/Socket.cpp
@@ -109,9 +109,11 @@ IoResult Socket::send(const void *buf, size_t len)
   if (n > 0)
     return IoResult(IO_OK, n, 0);
   if (n == 0)
-    return IoResult(IO_CLOSED, 0, 0);
+    return IoResult(IO_OK, 0, 0);
   if (errno == EAGAIN || errno == EWOULDBLOCK)
     return IoResult(IO_WOULDBLOCK, 0, errno);
+  if (errno == EPIPE || errno == ECONNRESET)
+    return IoResult(IO_CLOSED, 0, errno);
   return IoResult(IO_ERROR, 0, errno);
 }
 
